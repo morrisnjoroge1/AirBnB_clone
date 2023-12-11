bd28ea9630b26a4f055025bbe0dd62e7d6566c91
morris <mnjoroge26439@gmail.com>
2023-12-11 09:59:55 -0800

    airbnb

--- models/base_model.py
+++ models/base_model.py
@@ -16,7 +16,7 @@ class BaseModel:
             *args(): unused
             *kwargs(dict): key and value pairs
         """
-        timeformat = Y-%m-%dT%H:%M:%S.%f
+        timeformat = 'Y-%m-%dT%H:%M:%S.%f'
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
